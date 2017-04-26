c8bf0a82a991c2919cb4a868037e650af7ca14bb
David Rolfs and Halle Tempest <david@>
2017-04-26 13:46:06 -0700

    add front-end forms to do subtraction, divide and multiply and return results on document

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,37 @@ $(document).ready(function(){
 		var number1 = parseInt($("input#add1").val());
 		var number2 = parseInt($("input#add2").val());
 
-		var result = add(number1, number2);
-		$("#show").text(result);
+		var addResult = add(number1, number2);
+		$("#show").text(addResult);
 	});
+
+	$("form#sub").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("input#sub1").val());
+		var number2 = parseInt($("input#sub2").val());
+
+		var subResult = subtraction(number1, number2);
+			$("#show").text(subResult);
+	});
+
+	$("form#mult").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("input#mult1").val());
+		var number2 = parseInt($("input#mult2").val());
+
+		var multResult = multiply(number1, number2);
+			$("#show").text(multResult);
+	});
+
+	$("form#divide").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("input#divide1").val());
+		var number2 = parseInt($("input#divide2").val());
+
+		var divideResult = divide(number1, number2);
+			$("#show").text(divideResult);
+
+		});
 });
 
 // var number1 = parseInt(prompt("Give me a number: "));
